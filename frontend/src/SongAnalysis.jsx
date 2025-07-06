1a67d1ea81907779fa9569ccafc0fa52fa240c8a
DarkAngel <marcelo@darkblue.tv>
2025-07-05 21:33:13 -0300

    front | cluster display (to be fixed)

--- frontend/src/SongAnalysis.jsx
+++ frontend/src/SongAnalysis.jsx
@@ -5,6 +5,7 @@ import Bar from "./Bar";
 export default function SongAnalysis({ songData, currentTime, setCurrentTime }) {
 
     const [beats, setBeats] = useState([]);
+    const [clusters, setClusters] = useState([]);
     const [currentBeat, setCurrentBeat] = useState(false);
     const [minEnergy, setMinEnergy] = useState(0);
     const [maxEnergy, setMaxEnergy] = useState(0);
@@ -24,6 +25,9 @@ export default function SongAnalysis({ songData, currentTime, setCurrentTime }) 
     useEffect(() => {
       if (songData.beats) {
         setBeats(songData.beats);
+      }
+      if (songData.clusters) {
+        setClusters(songData.clusters);
       }
     }, [songData]);
 
@@ -50,33 +54,88 @@ export default function SongAnalysis({ songData, currentTime, setCurrentTime }) 
       }
     }, [currentBeat]);
 
+    function getClustersForBeat(beatTime) {
+      const retval = [];
+      for(const cluster of clusters){
+
+        // get clusters where 'start' is in this beat time
+        const filteredClusters = cluster.clusters.filter(c => {
+          return c.start <= beatTime && c.end >= beatTime;
+        });
+        // add the cluster placeholder
+        retval.push({
+          stem: cluster.stem,
+          tags: filteredClusters
+        });
+      }
+      return retval;
+    }
+
     return (
     <>
       <h2 className="text-x2 font-bold mb-4">🤖 Song Analysis</h2>
       
-      {/* Drum Parts */}
+      {/* Beats and clusters */}
+      {clusters.length > 0 && (
       <div className="mt-4">
-        <h3 className="text-x1 font-bold mb-2">Drum Parts</h3>
-        <div className="overflow-x-auto">
-          <div className="px-1">
-            {songData.drums && songData.drums.map((_type, index) => (
+        <h3 className="text-x1 font-bold mb-2">clusters</h3>
+        <div className="overflow-x-auto" >
+            {beats.map((beat, index) => {
+              const _time = beat.time || -1;
+              const clustersForBeat = getClustersForBeat(_time);
+
+              return (
               <div key={index} className="flex items-center space-x-4">
-                <div className="text-xs text-gray-400">{_type['type']}</div>
-                <div className="flex space-x-1">
-                  {_type['time'].map((_time, regionIndex) => (
-                    <div key={regionIndex} 
-                         className={`px-2 py-1 rounded text-xs ${currentTime >= _time[0] ? 'bg-green-500' : 'bg-gray-900'}`} 
-                         onClick={() => setCurrentTime(_time[0])}>
-                      {_time[0]}<br />
-                      {_time[1].toFixed(2)}
-                    </div>
-                  ))}
-                </div>
+                {
+                  clustersForBeat.map((_cluster, clusterIndex) => {
+                    return (
+                      <div key={clusterIndex} className="text-xs">
+                        {_cluster.tags && _cluster.tags.map((c, cIndex) => (
+                          <div
+                            key={cIndex}
+                            className={`px-2 py-1 rounded text-xs ${currentTime >= c.start && currentTime <= c.end ? 'bg-green-500' : 'bg-gray-900'}`} 
+                              onClick={() => setCurrentTime(c.start)}>
+                                {c.cluster}
+                          </div>
+                        ))}
+                        {_cluster.stem}
+                      </div>
+                    )
+                  })
+                }
+                <div className="text-xs">{index}</div>
               </div>
-            ))}
-          </div>
+              )
+            })}
         </div>
       </div>
+      )}
+
+      {/* Drum Parts */}
+      {songData.drums?.length>0 && (
+        <div className="mt-4">
+          <h3 className="text-x1 font-bold mb-2">Drum Parts</h3>
+          <div className="overflow-x-auto">
+            <div className="px-1">
+              {songData.drums.map((_type, index) => (
+                <div key={index} className="flex items-center space-x-4">
+                  <div className="text-xs text-gray-400">{_type['type']}</div>
+                  <div className="flex space-x-1">
+                    {_type['time'].map((_time, regionIndex) => (
+                      <div key={regionIndex} 
+                          className={`px-2 py-1 rounded text-xs ${currentTime >= _time[0] ? 'bg-green-500' : 'bg-gray-900'}`} 
+                          onClick={() => setCurrentTime(_time[0])}>
+                        {_time[0]}<br />
+                        {_time[1].toFixed(2)}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
 
       {/* Song Beats */}
       <div className="mt-4">
