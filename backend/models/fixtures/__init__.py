2cda0f32aba6313e6c5a75a9b12da6c9904a7c9d
DarkAngel <marcelo@darkblue.tv>
2025-07-13 18:20:33 -0300

    FixturesListModel name

--- backend/models/fixtures/__init__.py
+++ backend/models/fixtures/__init__.py
@@ -7,6 +7,6 @@ This module contains the fixture models and related classes.
 from .fixture_model import FixtureModel
 from .rgb_parcan import RgbParcan
 from .moving_head import MovingHead
-from .fixtures_model import FixturesListModel
+from .fixtures_list_model import FixturesListModel
 
 __all__ = ['FixtureModel', 'RgbParcan', 'MovingHead', 'FixturesListModel']
